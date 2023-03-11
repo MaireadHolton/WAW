71d232716d3905baf9f189ed646f1dec7ab542f8
Mairead Holton <97246575+MaireadHolton@users.noreply.github.com>
2023-03-11 16:40:23 +0000

    Add files via upload

---
+++ controllers/location-controller.js
@@ -0,0 +1,40 @@
+import { LocationSpec } from "../models/joi-schemas.js";
+import { db } from "../models/db.js";
+
+export const locationController = {
+  index: {
+    handler: async function (request, h) {
+      const list = await db.listStore.getListById(request.params.id);
+      const location = await db.locationStore.getLocationById(request.params.locationid);
+      const viewData = {
+        title: "Edit location",
+        list: list,
+        location: location,
+      };
+      return h.view("location-view", viewData);
+    },
+  },
+
+  update: {
+    validate: {
+      payload: LocationSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("location-view", { title: "Edit location error", errors: error.details }).takeover().code(400);
+      },
+    },
+    handler: async function (request, h) {
+      const location = await db.locationStore.getLOcationById(request.params.locationid);
+      const newTrack = {
+        location: request.payload.location,
+        latitude: Number(request.payload.latitude),
+        longitude: Number(request.payload.longitude),
+        date: request.payload.date,
+        details: request.payload.details,
+        pictures: request.payload.pictures,
+      };
+      await db.locationStore.updateLocation(location, newLocation);
+      return h.redirect(`/list/${request.params.id}`);
+    },
+  },
+};
