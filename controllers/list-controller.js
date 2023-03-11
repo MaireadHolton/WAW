5ccb56690e8d41f6ce2f85d11f01b59664d3aa74
Mairead Holton <97246575+MaireadHolton@users.noreply.github.com>
2023-03-11 18:09:29 +0000

    Add files via upload

--- controllers/list-controller.js
+++ controllers/list-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { LocationSpec } from "../models/joi-schemas.js";
 
 export const listController = {
   index: {
@@ -13,10 +14,19 @@ export const listController = {
   },
 
   addLocation: {
+    validate: {
+      payload: LocationSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("list-view", { title: "Add location error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const list = await db.listStore.getListById(request.params.id);
       const newLocation = {
         location: request.payload.location,
+        latitude: request.payload.latitude,
+        longitude: request.payload.longitude,
         date: request.payload.date,
         details: request.payload.details,
         pictures: request.payload.pictures,
